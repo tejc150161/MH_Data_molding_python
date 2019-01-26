94e7176b8921153469dcbf4b1513ba768852a706
tejc150161 <39040455+tejc150161@users.noreply.github.com>
2019-01-26 21:49:16 +0900

    Delete download.py

--- download.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-
-def download():
-    url = "https://goo.gl/1kw79Y"
-    file_list = ['_id_data.csv', 'asahi_match_record.csv','yomiuri_match_record.csv','mainichi_match_record.csv']
-
-    for file in file_list:
-        urllib.request.urlretrieve(url, file)
-    return "保存しました"
